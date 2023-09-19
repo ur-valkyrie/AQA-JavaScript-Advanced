54d666b1e2310ffa54fe2f13e700f92849fdff0e
ur-valkyrie <castiel.andreeva@gmail.com>
2023-09-19 16:19:32 +0200

    Add new folder 'js-variables' and a file 'variables.js'

---
+++ js-variables/variables.js
@@ -0,0 +1,16 @@
+let adminName = "Vitaliia"
+let userName = "Valkyrie"
+
+console.log(typeof adminName)
+console.log(typeof userName)
+
+let testVariable = "awesome"
+let testVariable2 = "you got " + 12
+let testVariable3 = "points"
+
+console.log(`Javascript is: ${testVariable} ${testVariable2} ${testVariable3}`)
+
+let animalName = "cat"
+animalName = "dog"
+
+console.log(animalName)
