dc185955d72fd852d59c1cdf6a74547867c43bf7
ur-valkyrie <83160853+ur-valkyrie@users.noreply.github.com>
2023-10-06 10:50:38 +0200

    Update functions-arrays.js

--- js-functions-arrays/functions-arrays.js
+++ js-functions-arrays/functions-arrays.js
@@ -1,6 +1,6 @@
 // Task 1
 function comparisonFirst(a, b) {
-    return a > b ? a : b 
+    return a > b ? a : b; 
 }
 
 const result1 = comparisonFirst(7, 4);
@@ -8,9 +8,7 @@ console.log("Function Declaration result:", result1);
 
 let comparisonSecond = function(a, b) {
     return compareNumbers =
-    a > b 
-        ? a
-        : b 
+    a > b ? a : b;
 };
 
 const result2 = comparisonSecond(10, 2);
@@ -18,12 +16,10 @@ console.log("Function Expression result:", result2);
 
 let comparisonThird = (a, b) => {
     return compareNumbers =
-    a > b 
-        ? a
-        : b 
+    a > b ? a : b; 
 };
 
-var result3 = comparisonThird(2, 3);
+let result3 = comparisonThird(2, 3);
 console.log("Arrow Function result:", result3);
 
 // Task 2
@@ -94,20 +90,20 @@ const array = [1, 8, 10, 11, -8, 17, 20, 22, 40, 37, 45, 0, 3, 45, 56];
 const oddNumbers = array.filter(number => number % 2 !== 0);
 console.log(oddNumbers);
 
-const arrayColors = ["yellow", "green", "apple", "red", "pink"]
-const sortedArrayColors = arrayColors.sort((a, b) => a.localeCompare(b))
-console.log(sortedArrayColors)
+const arrayColors = ["yellow", "green", "apple", "red", "pink"];
+const sortedArrayColors = arrayColors.sort((a, b) => a.localeCompare(b));
+console.log(sortedArrayColors);
 
 // Task 6
-const arrCapitals = ["Berlin", "Dublin", "Madrid", "Rome", "Oslo"]
-const arrCounties = ["Germany", "Ireland", "Spain", "Italy", "Norway"]
-const capitalsCountries = arrCapitals.concat(arrCounties)
-console.log(capitalsCountries)
+const arrCapitals = ["Berlin", "Dublin", "Madrid", "Rome", "Oslo"];
+const arrCounties = ["Germany", "Ireland", "Spain", "Italy", "Norway"];
+const capitalsCountries = arrCapitals.concat(arrCounties);
+console.log(capitalsCountries);
 
 // Task 7
-const arrNumbers = [10, 20, 30, 40, 50]
+const arrNumbers = [10, 20, 30, 40, 50];
 const sum = arrNumbers.reduce((value, i) => {
-    return value + i;
+    value + i;
 }, 0);
-console.log(sum)
+console.log(sum);
 
